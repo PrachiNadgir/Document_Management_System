f62a4a91207006101c46051a8163040e09c2cff9
Prachi Nadgir <nadgirprachi@gmail.com>
2026-04-14 19:46:13 +0530

    added UserContext,browserrouter,googleAuth

--- DocFrontend/src/main.jsx
+++ DocFrontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UserContext from './Context/UserContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <UserContext>
+
+      {/* ✅ Google Provider (optional but needed for Google login) */}
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+
+        {/* ✅ Toast global */}
+        <Toaster position="top-center" />
+
+        <App />
+
+      </GoogleOAuthProvider>
+
+    </UserContext>
+  </BrowserRouter>
+);
